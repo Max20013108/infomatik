3481c3ee9df5856823c0cfd9299e85c9edb4e462
Max20013108 <detlofs@gmail.com>
2019-12-12 09:07:40 +0100

    update files

--- Recursion/hanoi.java
+++ Recursion/hanoi.java
@@ -19,21 +19,20 @@ public static void move(char f, char t){
     System.out.println("Bewege scheibe von "+f+" zu "+t);
 }
 
-public static void hanoitower(int i,char f,char h,char t){
-    if(i == 0){
+public static void hanoitower(int n,char f,char h,char t){
+    if(n > 0){
+        hanoitower(n-1,f,t,h);
+        move(f,t);
+        hanoitower(n-1,h,f,t);
     }
-else{
-    hanoitower(i-1,f,t,h);
-    move(f,t);
-    hanoitower(i-1,h,f,t);
-
 }
 
-}
+
 
 public static void main(String[] args){
 
-    hanoitower(5, 'A', 'B', 'C');
+    hanoitower(10, 'A', 'B', 'C');
+    System.out.println(number(10));
 }
 
 
